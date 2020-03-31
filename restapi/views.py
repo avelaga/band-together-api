0c8aca5f97ac266f5f59e391f3cbc823bae43fea
Adam Martin <ammartin256@gmail.com>
2020-03-31 10:09:53 -0500

    Removed unneeded code

--- restapi/views.py
+++ restapi/views.py
@@ -14,11 +14,6 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework import status
 
-class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 10
-
 '''
 API endpoints for artist
 '''
