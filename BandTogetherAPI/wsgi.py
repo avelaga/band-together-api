f56856ce4fbb8d7c3fd842ced68e46e0c2030d96
Jason Moy <jason.moy42@gmail.com>
2020-03-31 02:15:47 +0000

    wsgi.py needs this to run

--- BandTogetherAPI/wsgi.py
+++ BandTogetherAPI/wsgi.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+path = '/code'
+if path not in sys.path:
+    sys.path.append(path)
+    
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BandTogetherAPI.settings')
