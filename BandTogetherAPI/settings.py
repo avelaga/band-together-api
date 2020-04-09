582980622ab9d571ebf152f471e1803d94601c3d
Jason Moy <jason.moy42@gmail.com>
2020-04-09 02:42:16 +0000

    Postgres settings.py

--- BandTogetherAPI/settings.py
+++ BandTogetherAPI/settings.py
@@ -87,10 +87,19 @@ WSGI_APPLICATION = 'BandTogetherAPI.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'band-together-db.cp7swib6datx.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
+
 }
 
 
