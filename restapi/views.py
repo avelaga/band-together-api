ad4ae7d17d9b56410739ca16ec1f679792db0b4e
Adam Martin <ammartin256@gmail.com>
2020-04-11 21:16:15 -0500

    Removed unnecessary mixin

--- restapi/views.py
+++ restapi/views.py
@@ -26,36 +26,6 @@ from rest_framework.settings import api_settings
 """
 API endpoints for artist
 """
-class MyPaginationMixin(object):
-    @property
-    def paginator(self):
-        """
-        The paginator instance associated with the view, or `None`.
-        """
-        if not hasattr(self, "_paginator"):
-            if self.pagination_class is None:
-                self._paginator = None
-            else:
-                self._paginator = self.pagination_class()
-        return self._paginator
-
-    def paginate_queryset(self, queryset):
-        """
-        Return a single page of results, or `None` if pagination 
-        is disabled.
-        """
-        if self.paginator is None:
-            return None
-        return self.paginator.paginate_queryset(queryset, self.request, view=self)
-
-    def get_paginated_response(self, data):
-        """
-        Return a paginated style `Response` object for the given 
-        output data.
-        """
-        assert self.paginator is not None
-        return self.paginator.get_paginated_response(data)
-
 class ArtistList(generics.ListAPIView):
     queryset = Artist.objects.all()
     serializer_class = ArtistListSerializer
@@ -113,8 +83,6 @@ class ArtistDetail(APIView):
 """
 API endpoints for locations
 """
-
-
 class LocationList(generics.ListAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationListSerializer
@@ -152,8 +120,6 @@ class LocationDetail(generics.RetrieveAPIView):
 """
 API endpoints for concerts
 """
-
-
 class ConcertList(generics.ListAPIView):
     queryset = Concert.objects.all()
     serializer_class = ConcertSerializer
@@ -167,8 +133,6 @@ class ConcertDetail(generics.RetrieveAPIView):
 """
 API endpoints for venues
 """
-
-
 class VenueList(generics.ListAPIView):
     queryset = Venue.objects.all()
     serializer_class = VenueSerializer
@@ -177,9 +141,6 @@ class VenueList(generics.ListAPIView):
 class VenueDetail(generics.RetrieveAPIView):
     queryset = Venue.objects.all()
     serializer_class = VenueSerializer
-
-
-
 
 
 # Calling this function will scrape the API's and load up our database
